fbe1cf1e81310837037d7de07ad6bedcc1786d56
Pieter Deane <deanepieter@gmail.com>
2024-07-24 21:53:28 +0200

    Refactor StoryList component to improve readability and handle loading state

--- src/story-list.jsx
+++ src/story-list.jsx
@@ -1,33 +1,44 @@
-import React, {useEffect, useState}from "react";
- 
- 
+import React, { useEffect, useState } from "react";
+
 export default function StoryList() {
     const [stories, setStories] = useState([]);
+    const [loading , setIsLoading] = useState(false)
 
     useEffect(() => {
-        fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-        .then(response => response.json())
-        .then(storyIds => {
-            const promises = storyIds.slice(0, 10).map(async storyId => {
-                const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`);
-                return await response.json();
-            });
-            Promise.all(promises).then(stories => {
+        const fetchStories = async () => {
+            setIsLoading(true)
+            try {
+                const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+                const storyIds = await response.json();
+                const promises = storyIds.slice(0, 20).map(storyId =>
+                    fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`).then(res => res.json())
+                );
+                const stories = await Promise.all(promises);
                 setStories(stories);
-            });
-        });
+            } catch (error) {
+                console.error("Failed to fetch stories", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchStories();
     }, []);
+
     return (
         <>
-        <h2>Top stories from hackerrank</h2>
-
-        <ul>
-            {stories.map(story => (
-                <li key={story.id}>{story.title}</li>
-            )
+            <h2>Top stories from Hacker News</h2>
+            {loading ? (
+                <p>Loading...</p>
+            ):
+            (
+            <ul>
+                {stories.map(story => (
+                    <li key={story.id}>{story.title}</li>
+                ))}
+            </ul>
             )}
-        </ul>
-        
         </>
-    )
+
+    );
+
 }
